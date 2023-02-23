218eb6241502e275b6b474707f02c1f6390dbc01
Retr0 <isaquemedeirosxx@gmail.com>
2023-02-23 11:18:26 -0300

    Deploy AWS amplify

--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -16,6 +16,7 @@ const Pages = () => {
       </Routes>
       <Rodape />
     </Hash>
+   
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/LandingP7/',
+  base: './',
   server: {
     port: 3000,
   },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+},
   build: {
     chunkSizeWarningLimit: 1600,
   },
-})
+  define: {
+    "global": 'globalThis',
+  },
+});
