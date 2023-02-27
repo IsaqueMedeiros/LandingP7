6ffcc31fb9f2503beb532b7781fa76c4900800e2
Retr0 <isaquemedeirosxx@gmail.com>
2023-02-27 11:19:18 -0300

    fixed erros nav mobile

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'tw-elements';
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
