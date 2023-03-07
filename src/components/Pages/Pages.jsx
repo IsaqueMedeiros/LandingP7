4139a8fa448fa7c4a25fbf4036ce9f47d214e8bb
Retr0 <isaquemedeirosxx@gmail.com>
2023-03-07 14:06:22 -0300

    no message

--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Hash, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Hash, Routes, Route,  } from "react-router-dom";
 import NavBar from "../NavBar/Navbar";
 import Rodape from "../Rodape/Rodape";
 import Index from "../Index/Index";
@@ -11,8 +11,8 @@ const Pages = () => {
     <Hash>
       <NavBar />
       <Routes>
-      <Route exact path="/" element={<Index />} />
-      <Route path="*" element={<ErrorPage/>} />
+      <Route path="/" element={<Index />} />
+      <Route path="/*" element={<ErrorPage />} />
       </Routes>
       <Rodape />
     </Hash>
